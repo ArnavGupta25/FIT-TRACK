74377f187187a7e785b8cdd64c52d71d0729c1ae
ARNAV GUPTA <arnavgupta1551@gmail.com>
2024-09-14 18:35:21 +0530

    updated css of login page, separate js for index

---
+++ index.js
@@ -0,0 +1,6 @@
+document.getElementById('loginBtn').addEventListener('click', function() {
+    window.location.href = 'auth.html';
+});
+document.getElementById('getstartedBtn').addEventListener('click', function() {
+    window.location.href = 'auth.html';
+});
